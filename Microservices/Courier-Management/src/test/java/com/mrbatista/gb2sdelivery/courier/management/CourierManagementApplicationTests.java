516ba46e60cf3bc17e4e85cf84652d8e396127fa
geibatista <geibatista@hotmail.com>
2026-01-14 20:34:23 -0300

    commit inicial

---
+++ Microservices/Courier-Management/src/test/java/com/mrbatista/gb2sdelivery/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbatista.gb2sdelivery.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/test/java/com/mrbatista/gb2sdelivery/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbatista.gb2sdelivery.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
